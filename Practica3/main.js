a9d630aae313db5dcb153311d352613654857697
josemauricioacev <122044542@upq.edu.mx>
2025-05-21 20:17:08 -0600

    Práctica 3 - Ejercicio a)

---
+++ Practica3/main.js
@@ -0,0 +1,7 @@
+const restar = require('./utils');
+
+console.log("Prueba 1: 100 - 52 =", restar(100, 52));   
+console.log("Prueba 2: 78 - 24 =", restar(78, 24));      
+console.log("Prueba 3: 0 - 3 =", restar(0, 3));       
+console.log("Prueba 4: -5 - (-3) =", restar(-5, -3));  
+console.log("Prueba 5: 1000 - 50 =", restar(1000, 50));  
---
+++ Practica3/utils.js
@@ -0,0 +1,9 @@
+//funcion retorne la respuesta de dos numeros
+function restar(a, b){
+    return a - b;
+}
+
+module.exports = restar;
+//Resultado
+console.log(restar(20,7));
+
