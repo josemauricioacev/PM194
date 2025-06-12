d9bedfa39d58de2c3479106c32090b69f4b8eb14
josemauricioacev <122044542@upq.edu.mx>
2025-06-11 19:08:23 -0600

    Componente propio Texto y su propiedad Contenido

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,28 @@
+/* Zona 1: Importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const Texto = (props) => {
+  const {contenido}=props;
+  return (
+    <Text>{contenido}</Text>
+  )
+};
+
+/* Zona 2: Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="hola"></Texto>
+      <Texto contenido="mundo"></Texto>
+      <Texto contenido="desde React Native"></Texto>
+      <Button title="Presioname"></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* Zona 3: Estética */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
