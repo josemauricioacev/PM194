a831eda09b802dfbe8d5caee14b42a7737a98f51
josemauricioacev <122044542@upq.edu.mx>
2025-06-13 20:09:01 -0600

    Práctica No.6: StyleSheet & Flex

--- my-blank/App.js
+++ my-blank/App.js
@@ -4,13 +4,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React,{useState} from 'react';
 
-const Texto=() => {
+const Texto=({style}) => {
   const [contenido,setContenido]=useState("Hola Mundo, React Native");
   const actualizaTexto=()=>{setContenido("Texto actualizado");}
   return (
-    <Text onPress={actualizaTexto}>{contenido}</Text>
+    <Text style={[styles.text, style]} onPress={actualizaTexto}>{contenido}</Text>
   );
-}
+};
 
 
 // Zona 2, MAIN (ejecucuión del programa)
@@ -19,13 +19,10 @@ export default function App() {
     <View style={styles.container}>
 
 
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Text>Hola Mundo, React Native</Text>
-      <Button title="Tlabaja"></Button>
-       
+      <Texto style={styles.red}></Texto>
+      <Texto style={styles.blue}></Texto>
+      <Texto style={styles.green}></Texto>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -37,8 +34,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
+    alignItems: 'baseline',
     justifyContent: 'center',
+    flexDirection: 'row-reverse',
   },
+  text:{
+    color: 'white',
+    fontSize:27,
+  },
+  red:{backgroundColor:'red'},
+  blue:{backgroundColor:'blue'},
+  green:{backgroundColor:'green'},
 });
